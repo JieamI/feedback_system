65c6b74301fd0698f849d5db46010b37d5982f1f
Moon-code <1732615826@qq.com>
2019-11-22 23:35:38 +0800

    feedback_system

--- GUI_test/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtCore,QtWidgets,QtGui
-import GUI
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    ui = GUI.Ui_MainWindow()
-    ui.show()
-    sys.exit(app.exec_())
